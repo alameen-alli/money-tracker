45076d4024c2d66c32bf9751758215f96bd89a41
Alameen8739 <allialameen8739@gmail.com>
2023-02-13 15:35:27 +0100

    html setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 function App() {
   return (
     <div className="App">
+      <main>
       <h1>$400.00</h1>
       <form>
         <div className="basic">
@@ -13,6 +14,39 @@ function App() {
         </div>
         <button type="submit">Add new transaction</button>
       </form>
+      <div className="transactions">
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Samsung TV</div>
+            <div className="description">it was time for new TV</div>
+          </div>
+          <div className="right">
+            <div className="price">$500</div>
+            <div className="datetime">02/13/2023, 03:27</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Samsung TV</div>
+            <div className="description">it was time for new TV</div>
+          </div>
+          <div className="right">
+            <div className="price">$500</div>
+            <div className="datetime">02/13/2023, 03:27</div>
+          </div>
+        </div>
+        <div className="transaction">
+          <div className="left">
+            <div className="name">New Samsung TV</div>
+            <div className="description">it was time for new TV</div>
+          </div>
+          <div className="right">
+            <div className="price">$500</div>
+            <div className="datetime">02/13/2023, 03:27</div>
+          </div>
+        </div>
+      </div>
+      </main>
     </div>
   );
 }
